8a72de121d4b7260643df7439381848e7f3df262
itsAnian <anian.seidl@gmail.com>
2025-03-04 10:43:36 +0100

    Add cJSON, Remove struct, Adjust CMake
    struct not needed, cause cJson creates a object, that can be written
    into

--- main.c
+++ main.c
@@ -1,41 +1,64 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
+#include "cJSON.h"
 
-const int argnumber = 1;
+const int argnumber = 1; const char *todolist = "todo.json";
+const char *todolist_history = "todo_history.json";
 
-struct todostruct{
-    int id;
-    char* titel;
-    char* description;
-    char* flags[4];
-};
+void Operation(int id, char *titel, char *description, char *flags[], int flagCount, int operation){
+    if(operation == 0){
+        printf("abfangen");
+    }
+    if(operation == 1){
+        printf("ADD-Operation");
+        cJSON *json = cJSON_CreateObject();
+        cJSON_AddNumberToObject(json, "id", id);
+        cJSON_AddStringToObject(json, "titel", titel);
+        cJSON_AddStringToObject(json, "description", description);
+        cJSON *flagsArray = cJSON_CreateArray();
+        for (int i = 0; i < flagCount; i++) {
+            cJSON_AddItemToArray(flagsArray, cJSON_CreateString(flags[i]));
+        }
+        cJSON_AddItemToObject(json, "flags", flagsArray);
 
+        char *jsonString = cJSON_Print(json);
+        printf("JSON:\n%s\n", jsonString);
+    }
+    if(operation == 2){}
+    if(operation == 3){}
+    if(operation == 4){}
+    if(operation == 5){}
+}
 
 int main(int argc, char* argv[])
 {
-    struct todostruct todo;
     int id;
-    char* titel;
-    char* description;
-    char* flags[argc];
+    char *titel;
+    char *description;
+    char *flags[argc];
     int flagCount = 0;
-    int state = 0;
+    int operation = 0;
 
     if(strcmp(argv[argnumber], "add") == 0){
         printf("ADD\n");
-        state = 1;
+        operation = 1;
     }
     if(strcmp(argv[argnumber], "edit") == 0){
         printf("EDIT\n");
-        state = 2;
+        operation = 2;
     }
     if(strcmp(argv[argnumber], "remove") == 0){
         printf("REMOVE\n");
-        state = 3;
+        operation = 3;
     }
     if(strcmp(argv[argnumber], "finish") == 0){
         printf("FINISH\n");
-        state = 4;
+        operation = 4;
+    }
+    if(strcmp(argv[argnumber], "list") == 0){
+        printf("LIST\n");
+        operation = 4;
     }
 
     for (int i = 0; i < argc; i++) {
@@ -44,24 +67,16 @@ int main(int argc, char* argv[])
         }
         if(strcmp(argv[i], "-d") == 0){
             description = argv[i+1];
-        }
+        } 
         if(argv[i][0] == '#'){
             flags[flagCount] = argv[i];
             flagCount++;
         }
     }
 
-    todo.id = 187;
-    todo.titel = titel;
-    todo.description = description;
-    for (int i = 0; i < flagCount; i++) {
-        todo.flags[i] = flags[i];
-    }
-
-    printf("id: %d\ntitel: %s\ndescription: %s\n", todo.id, todo.titel, todo.description);
-    for (int i = 0; i < flagCount; i++) {
-        printf("flags: %s\n", todo.flags[i]);
-    }
+    Operation(id, titel, description, flags, flagCount, operation);
 
     return 0;
 }
+
+
